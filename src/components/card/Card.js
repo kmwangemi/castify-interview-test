89702bc743e93253d137e9bc27a7fef0a6df23e9
kmwangemi <mwangemik@gmail.com>
2022-01-31 16:33:16 +0300

    add alert on click

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,8 +2,15 @@ import './card.css';
 
 const Card = ({ album }) => {
 
+   const handleClick = () => {
+      if (!album.description) {
+         alert('No desription')
+      }
+      alert(album.description)
+   }
+
    return (
-      <div className='card__Wrapper'>
+      <div className='card__Wrapper' onClick={handleClick}>
          <img src={album.thumbnail} alt={album.title} loading='lazy' />
          <div className='title__Wrapper'>{album.title}</div>
       </div>
